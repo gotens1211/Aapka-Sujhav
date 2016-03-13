982b1672e04e474ef2ed245af6136d6f52ec7d7b
gotens1211 <nikeythe@gmail.com>
2016-03-13 12:01:20 +0530

    Custom css components

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+//Animation at the start
+var $bg=$('<div id="im"></div>');
+$("body").append($bg);
+$($bg).fadeOut(5000);
+
+//Hide the menu bar when on a page other the home
+//Append a button on the menu bar 
+//Show the menu bar when clicked on the button
+//Hide it again after some timeq
